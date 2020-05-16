53fbb68eacf661ca55c0974c71f52eee71461ccc
Matine temory <temorymatine@gmail.com>
2020-05-16 14:42:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,6 @@
 # don't forget to add: require 'pry'
 
+
 def get_user_input
   gets.chomp
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
   else
     string
+    binding.pry
   end
 end
